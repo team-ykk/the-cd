a2b1e9a7af4192fc2dba3d66472a42d7df10ca46
abetakashi1004 <takashi.48694062@gmail.com>
2019-11-16 06:33:35 +0000

    [Edit]devise_forの位置を修正

--- app/controllers/publics/items_controller.rb
+++ app/controllers/publics/items_controller.rb
@@ -1,10 +1,20 @@
 class Publics::ItemsController < Publics::ApplicationController
+
   def index
+  	@items = Item.page(params[:page]).reverse_order
   end
 
   def show
+  	@item = Item.find(params[:id])
   end
 
   def ranking
   end
+
+  private
+  def item_params
+        params.require(:item).permit(:item_name, :shipdate, :price, :stock, :status, :item_profile, :deleted_at)
+  end
+
+
 end
