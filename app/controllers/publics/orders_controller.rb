65768926340556ed49df531e7ccab0a4c978cf54
kawasaki-daisuke <asas15769@gmail.com>
2019-11-20 06:23:24 +0000

    kawasaki

--- app/controllers/publics/orders_controller.rb
+++ app/controllers/publics/orders_controller.rb
@@ -1,8 +1,11 @@
 class Publics::OrdersController < Publics::ApplicationController
   def index
+  	
   end
 
   def new
+  	@enduser = Enduser.where(id: current_enduser.id)
+  	@order = Order.new
   end
 
   def create
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
 	belongs_to :enduser
 	belongs_to :address
 	has_many :histories, dependent: :destroy
+	enum payment: [['銀行振込',1], ['クレジットカード',2], ['代引き',3]]
 end
