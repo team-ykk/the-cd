80ec1d59344c555e47dcb464239d2e3ebd76bebd
yuma-oji <yumhouse.a@gmail.com>
2019-11-20 13:34:22 +0900

    [add]管理者側:商品追加機能/入荷機能/商品詳細機能/商品一覧機能/

--- app/controllers/admins/additems_controller.rb
+++ app/controllers/admins/additems_controller.rb
@@ -1,10 +1,24 @@
 class Admins::AdditemsController < Admins::ApplicationController
   def new
+  	@items = Item.search(params[:search])
+  	@item = @items.page(params[:page])
+  	@additem = Additem.new
+  	render :new
   end
-
   def index
   end
 
   def create
+  	@additem = Additem.new(additem_params)
+  	@additem.save
+  	@item = Item.find(@additem.item_id)
+  	@item_stock = @additem.add_item + @item.stock
+  	@item.update(stock:@item_stock)
+  	@item.save
+  	redirect_to admins_additems_path
+  end
+  private
+  def additem_params
+  	params.require(:additem).permit(:add_item, :item_id)
   end
 end
--- app/controllers/admins/freights_controller.rb
+++ app/controllers/admins/freights_controller.rb
@@ -1,7 +1,18 @@
 class Admins::FreightsController < Admins::ApplicationController
+  def index
+  	@freight = Freight.find(1)
+  end
   def edit
+  	@freight = Freight.find(1)
   end
 
   def update
+  	@freight = Freight.find(1)
+  	@freight.update(freight_params)
+  	redirect_to admins_freights_path
+  end
+  private
+  def freight_params
+  	params.require(:freight).permit(:postage)
   end
 end
