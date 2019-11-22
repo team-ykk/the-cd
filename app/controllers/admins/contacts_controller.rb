60125513657aeda67b8404fabfba9a9aa1b33d35
井上勇義 <inoueyuugi@inoueyuuginoMacBook-Pro.local>
2019-11-22 19:19:36 +0900

    問い合わせメール機能完了

--- app/controllers/admins/contacts_controller.rb
+++ app/controllers/admins/contacts_controller.rb
@@ -2,4 +2,9 @@ class Admins::ContactsController < Admins::ApplicationController
   def index
   	  @contact = Contact.page(params[:page]).reverse_order
   end
+
+  def show
+  	  @contact = Contact.find(params[:id])
+      @reply = ContactReply.find_by(contact_id: @contact.id)
+  end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,8 @@
 class ContactMailer < ApplicationMailer
 
-  	def send_when_admin_reply(enduser, contact_reply)
+  	def send_when_admin_reply(enduser, reply_content)#メールを送る際の必要な情報を引数として入れてあげる。
   		@enduser = enduser #ユーザー情報
-  		@reply = contact_reply.reply_content #返信内容
+  		@answer = reply_content #返信内容
   		mail to: enduser.email, subject: '[THE CD]お問い合わせありがとうございます'
   	end
 end
