dc03f3539f73e7c1a64c47324dfd3c00988c6180
kawasaki-daisuke <asas15769@gmail.com>
2019-11-22 08:02:18 +0000

    オーダー機能追加中

--- app/controllers/publics/orders_controller.rb
+++ app/controllers/publics/orders_controller.rb
@@ -1,10 +1,14 @@
 class Publics::OrdersController < Publics::ApplicationController
+  
+  #購入履歴画面にもデータを保存させる
+  #↓
+  #カートを空にする機能を作る
   def index
   	
   end
 
   def new
-  	@enduser = Enduser.where(id: current_enduser.id)
+  	@enduser = Enduser.where(id: current_enduser.id)#ここでエラーが起こったら、ログインする
   	@order = Order.new
 
   end
@@ -13,6 +17,20 @@ class Publics::OrdersController < Publics::ApplicationController
     @order = Order.new(order_params)
     @order.enduser_id = current_enduser.id
     @order.save
+
+    
+
+    @history = History.new
+    @history.order_id = current_order.id
+    #@history.item_name = items.item_name
+    #@history.artist_name = artists.artist_name
+    #@history.quantity = cart_items.quantity
+    #@history.subtotal = cart_items.subtotal
+    #@history.item_profile_id = items.item_profile_id
+    @history.save
+    
+
+
     redirect_to orders_complete_path
   end
 
