5b96b85a8c087aec75b545dd696d600b457efa29
井上勇義 <inoueyuugi@inoueyuuginoMacBook-Pro.local>
2019-11-25 21:23:10 +0900

    受注履歴一覧の作成完了

--- app/controllers/admins/contacts_controller.rb
+++ app/controllers/admins/contacts_controller.rb
@@ -1,6 +1,6 @@
 class Admins::ContactsController < Admins::ApplicationController
   def index
-  	  @contact = Contact.page(params[:page]).reverse_order
+  	  @contacts = Contact.page(params[:page]).reverse_order
   end
 
   def show
